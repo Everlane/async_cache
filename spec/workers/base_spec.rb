8c0220285cdd2824eafb1ffc0e8d6d8b499650af
Dirk Gadsden <dirk@esherido.com>
2015-12-16 17:49:24 -0800

    Add specs for base worker perform implementation

--- spec/workers/base_spec.rb
+++ spec/workers/base_spec.rb
@@ -14,4 +14,29 @@ describe AsyncCache::Workers do
       expect(subject.worker_for_name :active_job).to eql AsyncCache::Workers::ActiveJobWorker
     end
   end
+
+  describe '#perform' do
+    class TestWorker
+      include AsyncCache::Workers::Base
+    end
+
+    subject do
+      TestWorker.new
+    end
+
+    it 'evaluates the job and writes to the backend' do
+      backend = spy 'Backend'
+      allow(AsyncCache).to receive(:backend).and_return(backend)
+
+      key             = 'test'
+      version         = 42
+      expires_in      = 1337
+      block_arguments = [1]
+      block_source    = 'proc { |arg| arg * 2 }'
+
+      expect(backend).to receive(:write).with(key, [2, version], expires_in: expires_in)
+
+      subject.perform key, version, expires_in, block_arguments, block_source
+    end
+  end
 end
