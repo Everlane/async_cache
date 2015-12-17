4616af2630eab70566e472a6cd2cb63aacb6776e
Dirk Gadsden <dirk@esherido.com>
2015-12-16 17:42:13 -0800

    Use SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,11 @@ require 'bundler/setup'
 require 'rails'
 require 'sidekiq'
 require 'sidekiq/testing'
+require 'simplecov'
 
 Sidekiq::Testing.inline!
+SimpleCov.start
+
 Rails.cache  = ActiveSupport::Cache::MemoryStore.new
 Rails.logger = Logger.new($stdout).tap { |log| log.level = Logger::ERROR }
 
