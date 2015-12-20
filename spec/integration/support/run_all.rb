0532e6eeab4636de9bf73bf90dc37d1bf1d0124c
Dirk Gadsden <dirk@esherido.com>
2015-12-20 13:20:06 -0800

    Add script to run all servers for integration tests

---
+++ spec/integration/support/run_all.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+commands = [
+  'bundle exec sidekiq -r ./spec/integration/support/sidekiq.rb',
+  'bundle exec ruby spec/integration/support/sinatra.rb'
+]
+
+pids = commands.map do |c|
+  Process.spawn c
+end
+
+shutdown = proc { Process.kill 'TERM', *pids }
+
+Signal.trap 'INT',  &shutdown
+Signal.trap 'TERM', &shutdown
+
+Process.waitall
